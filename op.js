193e084cb0c9f47697effa590f6db4b2d29c96cc
c0linw <41310013+c0linw@users.noreply.github.com>
2024-10-09 15:03:47 -0700

    Update op.js

--- op.js
+++ op.js
@@ -6,7 +6,9 @@
     }
 
     function calculateMaxOP(chart) {
-        return (chart.levelNum + 3) * 5;
+        let songName = songsByID.get(chart.songID).title
+        let constant = constants.hasOwnProperty(songName) ? constants[songName] : chart.levelNum;
+        return (constant + 3) * 5;
     }
 
     function calculatePlayerOP(chart) {
@@ -27,20 +29,39 @@
                 break;
         }
         let scoreBonus = pb.scoreData.score > 1007500 ? (pb.scoreData.score - 1007500) * 0.0015 : 0;
+
+        let songName = songsByID.get(chart.songID).title
+
         let playRating = Math.min(pb.calculatedData.rating, chart.levelNum + 2);
+
+        // adjust play rating if version cc is different from latest cc
+        if (constants.hasOwnProperty(songName)) {
+            playRating -= chart.levelNum - constants[songName]
+        }
 
         return playRating * 5 + lampBonus + scoreBonus;
     }
+
+    const songsByID = new Map();
     const pbsByChartID = new Map();
     let maxOP = 0;
     let playerOP = 0;
     let playCount = 0;
 
-    const version = prompt('Select version (e.g. "paradiselost", "new", "newplus", "sun")', "sun");
-    if (version == null) {
-        alert("Please enter a version name");
+    const ccURLs = {
+        "newplus": "https://raw.githubusercontent.com/Dogeon188/chuni-tools/refs/heads/master/docs/data/song-const/newplus.json",
+        "sun": "https://raw.githubusercontent.com/Dogeon188/chuni-tools/refs/heads/master/docs/data/song-const/sun.json",
+        "sunplus": "https://raw.githubusercontent.com/Dogeon188/chuni-tools/refs/heads/master/docs/data/song-const/sunplus.json",
+        "luminous": "https://raw.githubusercontent.com/Dogeon188/chuni-tools/refs/heads/master/docs/data/song-const/luminous.json"
+    }
+    const version = prompt('Select version (supported versions: "newplus", "sun", "sunplus", "luminous")', "sun");
+    if (version == null || !ccURLs.hasOwnProperty(version)) {
+        alert("Unsupported game version");
         return;
     }
+
+    const constants = await fetch(ccURLs[version])
+        .then((r) => r.json());
 
     const allCharts = await fetch("https://raw.githubusercontent.com/zkrising/Tachi/refs/heads/main/seeds/collections/charts-chunithm.json")
         .then((r) => r.json())
@@ -53,6 +74,10 @@
     const { pbs, songs, charts } = await fetch(`/api/v1/users/${id}/games/chunithm/Single/pbs/all?alg=rating`)
         .then((r) => r.json())
         .then((r) => r.body);
+
+    for (const song of songs) {
+        songsByID.set(song.id, song);
+    }
 
     for (const pb of pbs) {
         pbsByChartID.set(pb.chartID, pb);
